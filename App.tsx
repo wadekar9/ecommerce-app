fed07035dfcfc47c1fbf226042c1659e518de736
wadekar9 <npwadekar7@gmail.com>
2026-05-24 17:09:39 +0530

    feat: implement splash screen functionality using react-native-bootsplash on Android and iOS

--- App.tsx
+++ App.tsx
@@ -3,8 +3,16 @@ import ThemeProvider from '$context/app-theme.context'
 import AppStackNavigator from '$navigation/app-stack-navigator.navigation'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import FlashMessage from 'react-native-flash-message'
+import BootSplash from "react-native-bootsplash";
 
 const App = () => {
+
+  React.useEffect(() => {
+    (async () => {
+      await BootSplash.hide({ fade: true })
+    })()
+  }, [])
+
   return (
     <ThemeProvider>
       <GestureHandlerRootView style={{ flex: 1 }}>
