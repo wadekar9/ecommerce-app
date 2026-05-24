856d68e93b37b3955ca941cc60e5df702eb1ed8b
wadekar9 <npwadekar7@gmail.com>
2026-05-24 16:03:46 +0530

    feat: implement persistent theme state using local storage in AppThemeContext

--- app/context/app-theme.context.tsx
+++ app/context/app-theme.context.tsx
@@ -1,13 +1,26 @@
+import { EStorageKeys } from "$constants/storage.constants";
 import { ITheme, IThemeContext } from "$types/common.types";
-import React, { createContext, useMemo, useState } from "react";
+import { Storage } from "$utils/storage";
+import React, { createContext, useEffect, useMemo, useState } from "react";
 
 export const AppThemeContext = createContext<IThemeContext | undefined>(undefined);
 
 const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
 
     const [theme, setTheme] = useState<ITheme>('light');
-    const changeTheme = () => setTheme(prev => prev === 'light' ? 'dark' : 'light');
+    const changeTheme = () => {
+        const newTheme = theme === 'light' ? 'dark' : 'light';
+        Storage.set(EStorageKeys.APP_THEME, newTheme);
+        setTheme(newTheme);
+    }
     const value = useMemo(() => ({ theme, changeTheme }), [theme]);
+
+    useEffect(() => {
+        (async () => {
+            const storageTheme = await Storage.getString(EStorageKeys.APP_THEME);
+            setTheme(storageTheme as ITheme || 'light');
+        })();
+    }, []);
 
     return (
         <AppThemeContext.Provider value={value}>
